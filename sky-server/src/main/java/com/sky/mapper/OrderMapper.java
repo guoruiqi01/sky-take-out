d9c526adb7dae5d1823574a651e505d93fad11d8
ruiqi guo <1811319761@qq.com>
2024-07-27 22:03:43 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -5,6 +5,10 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.core.annotation.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -45,4 +49,13 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+    /**
+     * 根据订单状态和下单时间查询订单
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{overTime} ")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime overTime);
+
+
 }

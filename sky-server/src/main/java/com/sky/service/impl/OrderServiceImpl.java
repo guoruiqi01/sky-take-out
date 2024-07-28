cae26d5188f791ed8d406051f71bc961e556cdca
ruiqi guo <1811319761@qq.com>
2024-07-28 13:26:31 +0800

    客户催单

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -519,4 +519,28 @@ public class OrderServiceImpl implements OrderService {
         orderMapper.update(orders);
     }
 
+    /**
+     * 客户催单
+     * @param id
+     */
+    @Override
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Map map = new HashMap();
+        map.put("type", 2);
+        map.put("orderId", ordersDB.getId());
+        map.put("content", "订单号" + ordersDB.getNumber());
+        String jsonString = JSONObject.toJSONString(map);
+
+        // 向浏览器推送消息
+        webSocketServer.sendToAllClient(jsonString);
+    }
+
 }
